b05f74db9beaac768f2edc0b74d472cccc0dfd2f
shiyu22 <cshiyu22@gmail.com>
2020-07-16 16:58:53 +0800

    add milvus

--- api/app/routers/related.py
+++ api/app/routers/related.py
@@ -37,7 +37,9 @@ async def get_related(request: Request, uid: str, page_number: int = 1, query_id
     # # Retrieve documents from HNSW.
     # labels, distances = related_searcher.hnsw.knn_query(source_vector, k=k)
 
+    vids = [float(vec) for vec in source_vector]
     print("--------source_vector",source_vector)
+
     parameters = {'ef': 64}
     status, results = related_searcher.milvus.search(related_searcher.collection_name, [source_vector], 100, parameters)
     print(status, results)
